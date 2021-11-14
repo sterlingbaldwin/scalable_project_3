0be482362c1ec310fd916ee6fa306fee968ebebe
sterlingbaldwin <sterling16@mac.com>
2021-11-14 13:55:22 +0000

    uses correct version of python

--- start_ship.sh
+++ start_ship.sh
@@ -4,13 +4,12 @@ cd projects/scalable_project_3
 source venv/bin/activate
 python3 -c "
 
-from Station import station
+from Ship import ship
 from numpy.random import random
 from uuid import uuid4
 
-s = station(
-    population=random(10, 100_00),
-    stationId=uuid4.hex(), 
+s = ship(
+    shipId=uuid4.hex(), 
     address=$1,
     port=$2)
 s.run()
