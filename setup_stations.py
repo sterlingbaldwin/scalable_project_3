d552189279b5c61787ddc4cbfb84a1860b8ceec8
sterlingbaldwin <sterling16@mac.com>
2021-11-14 15:17:36 +0000

    Splits the simulation starter and the station starter

--- setup_stations.py
+++ setup_stations.py
@@ -1,24 +1,12 @@
-import sys
 import os
-import argparse
 import paramiko
+import argparse
 from random import choice
-from multiprocessing import Process
-from Simulator import simulator
-
 
 # PI_ADDRESSES = ["10.35.70.29", "10.35.70.20"]
 PI_ADDRESSES = ["10.35.70.29"]
 SSH_KEY = f"{os.environ['HOME']}/.ssh/id_rsa.pub"
 USER = os.environ['USER']
-
-
-def run_simulator(world_size: int, address: str, port: str):
-    sim = simulator(
-        size=world_size,
-        host=address,
-        port=port)
-    sim()
 
 def setup_stations(num_stations: int, address: str, port: str):
     print("Starting station setup")
@@ -37,17 +25,9 @@ def setup_stations(num_stations: int, address: str, port: str):
         for line in stdout.readlines():
             print(line)
 
-def setup_ships(number):
-    pass
-
 def main():
     parser = argparse.ArgumentParser(
-        description="Simulate some space mail!")
-    parser.add_argument(
-        '--size',
-        type=int,
-        default=1e5,
-        help="The size of the simulated universe, default is 1e5 km")
+        description="Start the stations")
     parser.add_argument(
         "--host",
         type=str,
@@ -59,28 +39,15 @@ def main():
         default="5000",
         help="The port the simulator should run on, default is 5000")
     parser.add_argument(
-        "--num-ships",
-        type=int,
-        default=10,
-        help="Number of ships to spawn, default = 10")
-    parser.add_argument(
         "--num-stations",
         type=int,
         default=1,
         help="Number of stations to spawn, default = 1")
     args = parser.parse_args()
-    sim = simulator(
-        size=args.size,
-        host=args.host,
+    setup_stations(
+        num_stations=args.num_stations,
+        address=args.host,
         port=args.port)
-    sim()
-    # sim_proc = Process(target=run_simulator, args=(args.size, args.host, args.port))
-    # sim_proc.start()
-    # import ipdb; ipdb.set_trace()
-    # sim_proc.join()
-
-    # setup_stations(args.num_stations, args.host, args.port)
-    
 
 if __name__ == "__main__":
-    sys.exit(main())
+    main()
