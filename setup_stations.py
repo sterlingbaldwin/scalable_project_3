9aef1dae16987ec2548e7ad3da09b9e294948c5f
sterlingbaldwin <sterling16@mac.com>
2021-11-15 10:51:29 +0000

    disambiguates the addresses

--- setup_stations.py
+++ setup_stations.py
@@ -8,16 +8,16 @@ PI_ADDRESSES = ["10.35.70.29", "10.35.70.30"]
 SSH_KEY = f"{os.environ['HOME']}/.ssh/id_rsa.pub"
 USER = os.environ['USER']
 
-def setup_stations(num_stations: int, address: str, port: str):
+def setup_stations(num_stations: int, server_address: str, port: str):
     print("Starting station setup")
     for _ in range(num_stations):
-        address = choice(PI_ADDRESSES)
-        print(f"starting new station on {address}")
+        station_address = choice(PI_ADDRESSES)
+        print(f"starting new station on device {station_address}")
         client = paramiko.SSHClient()
         client.load_system_host_keys()
         client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-        client.connect(address, username=USER, key_filename=SSH_KEY)
-        cmd = f'bash ~/projects/scalable_project_3/start_station.sh {address} {port}'
+        client.connect(station_address, username=USER, key_filename=SSH_KEY)
+        cmd = f'bash ~/projects/scalable_project_3/start_station.sh {server_address} {port}'
         (_, stdout, stderr) = client.exec_command(cmd)
         
         for line in stderr.readlines():
@@ -32,7 +32,7 @@ def main():
         "--host",
         type=str,
         default="127.0.0.1",
-        help="The IP address to bind to, by default is set to 172.0.0.1. If you want anyone else to see the server run with 0.0.0.0")
+        help="The IP address that the simulator is running on")
     parser.add_argument(
         "--port",
         type=str,
