d05c3aa18a8adc259c03cd113870f915046de36f
Angkirat Sandhu <angkirat@gmail.com>
2021-11-24 14:37:52 +0000

    ship addition done

--- MainController.py
+++ MainController.py
@@ -1,33 +1,43 @@
+from flask import Flask, request
 from typing import Union
+import configparser
 import pandas as pd
 import numpy as np
 import datetime
 import re
 
+from setup_stations import main
+
 SHIP_DETAILS = pd.DataFrame(columns=['ShipID', 'port', 'Address', 'Speed', 'CommunicationRange', 'location', 'pingTime'])
 STATION_DETAILS = pd.DataFrame(columns=['StationID', 'port', 'Address', 'location', 'pingTime'])
 COMMUNICATION_TABLE = pd.DataFrame(columns=['Entity1Type', 'Entity1', 'Entity2Type', 'Entity2'])
 
-def addShip(shipID:str, port:str, address:str, speed:str, comRange:str, loc:tuple):
-    """Add new Ship details in the controler PI
+config = configparser.ConfigParser()
+config.read('Environment.ini')
+app = Flask(__name__)
 
-    Args:
-        shipID (str): New Ship ID
-        port (str): Entity port for Communication
-        address (str): Communication address of the ship
-        speed (str): Ship's Speed
-        comRange (str): Ship's Communication range
-        loc (tuple): Ships location
+def addShip():
+    """Add new Ship details in the controler PI
     """
-    SHIP_DETAILS.loc[SHIP_DETAILS.shape[0]] = {
-        'ShipID':shipID,
-        'port':port,
-        'Address':address,
-        'Speed':speed,
-        'CommunicationRange':comRange,
-        'location':'x:{}, y:{}'.format(loc[0], loc[1]),
-        'pingTime': datetime.datetime.now()
-    }
+    if request.method == 'POST':
+        inputShipDetails = request.json
+        inputShipDetails['pingTime'] = datetime.datetime.now()
+        print(inputShipDetails)
+        SHIP_DETAILS.loc[SHIP_DETAILS.shape[0]] = request.json
+        return f'Ship count is {SHIP_DETAILS.shape[0]}'
+    else:
+        return """
+        This is method needs to be run using post method with json entry similar to the one below:
+
+        {
+            'ShipID':shipID,
+            'port':port,
+            'Address':address,
+            'Speed':speed,
+            'CommunicationRange':comRange,
+            'location':'x:{}, y:{}'.format(loc[0], loc[1]), #here loc[0] coresponds to the x position and loc[1] the corresponding y
+        }
+        """
 
 def addStation(StationID:str, port:str, address:str, loc:tuple):
     """Add new Ship details in the controler PI
@@ -92,3 +102,15 @@ def communicationPairing():
                     'Entity2Type':'station',
                     'Entity2':STATION_DETAILS.loc[j, 'StationID']
                 }
+
+
+
+
+app.add_url_rule('/addShips', 'adding Ship', addShip, methods=['GET','POST'])
+
+if __name__=="__main__":
+    app.run(
+        host=config['MainController']['hostIP']
+        ,port=config['MainController']['port']
+        ,debug=config['MainController'].getboolean('debug')
+    )
