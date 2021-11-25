0c5f9b685a9e86fa69ef73871f30029689b4d17b
sterlingbaldwin <sterling16@mac.com>
2021-11-25 18:01:47 +0000

    Moves the loc to the entity class, adds connect request

--- Ship.py
+++ Ship.py
@@ -7,6 +7,7 @@ __version__ = 1.0
 from time import sleep
 from numpy.random import uniform
 from typing import List
+from uuid import uuid4
 import json
 
 from superEntity import SuperEntity
@@ -18,11 +19,10 @@ class ship(SuperEntity):
     """Ship Class
     Args:
         loc(tuple): x and y cordinate of the Ships
-        ShipID(str): the name of the ship
+        ship_id(str): the name of the ship
     """
-    def __init__(self, shipId: str, simulator_address: str, port: str) -> None:
-        super().__init__(shipId, port, simulator_address)
-        self.__loc = (0.0, 0.0)
+    def __init__(self, ship_id: str, simulator_address: str, port: str) -> None:
+        super().__init__(ship_id, port, simulator_address)
         self.__range = 0
         self.__speed = 0
         self.__itinerary = []
@@ -31,27 +31,6 @@ class ship(SuperEntity):
     @property
     def itinerary(self):
         return self.__itinerary
-
-    @property
-    def loc(self):
-        """Getter fuction for the location property
-
-        Returns:
-            tuple[float]: x and y cordinates of the ships.
-        """
-        return self.__loc
-    
-    @loc.setter
-    def loc(self, inp: tuple):
-        """Setter fuction for the location property
-
-        Args:
-            inp (tuple[float]): (x, y) type input
-        """
-        if len(inp) == 2:
-            self.__loc = inp
-        else:
-            raise ValueError('Input type expected in the form of x,y')
     
     @property
     def range(self):
@@ -85,6 +64,15 @@ class ship(SuperEntity):
         data = json.loads(res.content)
         print(f"ping response {data}...")
         return data
+    
+    def connect(self):
+        params = {
+            'ship_id': self._id,
+            'speed': self.__speed,
+            'comRange': self.__range,
+            'loc': self.loc
+        }
+        return self.make_request_to_controller('add_ship', params, method="POST")
 
 if __name__ == "__main__":
     pass
