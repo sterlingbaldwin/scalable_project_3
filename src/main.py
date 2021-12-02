2f3d870fe77e8861801b77ddb9130749c3aa2305
Sterling Baldwin <sterlingbaldwin@users.noreply.github.com>
2021-12-02 12:44:23 +0000

    derp

--- src/main.py
+++ src/main.py
@@ -10,6 +10,7 @@ from simulator import Simulator
 
 config = configparser.ConfigParser()
 config.read('../Environment.ini')
+config = config['config']
 
 def main():
     parser = argparse.ArgumentParser(
@@ -18,22 +19,22 @@ def main():
         '--size',
         type=int,
         default=config['size'],
-        help=f"The size of the simulated universe, default is {config['config']['size']} km")
+        help=f"The size of the simulated universe, default is {config['size']} km")
     parser.add_argument(
         "--host",
         type=str,
         default=config['hostIP'],
-        help=f"The IP address to bind to, by default is set to {config['config']['hostIP']}. If you want anyone else to see the server run with 0.0.0.0")
+        help=f"The IP address to bind to, by default is set to {config['hostIP']}. If you want anyone else to see the server run with 0.0.0.0")
     parser.add_argument(
         "--port",
         type=str,
         default=config['port'],
-        help=f"The port the simulator should run on, default is {config['config']['port']}")
+        help=f"The port the simulator should run on, default is {config['port']}")
     parser.add_argument(
         "--num-ships",
         type=int,
         default=config['num_ships'],
-        help=f"The number of ships to spawn, default is {config['config']['num_ships']}")
+        help=f"The number of ships to spawn, default is {config['num_ships']}")
     args = parser.parse_args()
     sim = Simulator(
         size=args.size,
